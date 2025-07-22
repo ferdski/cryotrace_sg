5c8e6e2f0dc34c09db91cbdb9d8efae129145934
Ubuntu <ubuntu@ip-172-31-15-229.us-east-2.compute.internal>
2025-07-22 17:12:20 +0000

    Initial integration of local code onto AWS

---
+++ client/declarations.d.ts
@@ -0,0 +1,9 @@
+// src/declarations.d.ts
+declare module './Sidebar';
+declare module './MainContent';
+declare module './components/UsersList';
+declare module './components/ContainersList';
+declare module './components/RecordsByDate';
+declare module './components/RecordsByLocation';
+declare module './components/RecordsByManifestId';
+declare module './components/RecordsAll';
