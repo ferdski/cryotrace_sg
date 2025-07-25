9ed7928c27cea3a812321413a003b227c1701c46
Ubuntu <ubuntu@ip-172-31-15-229.us-east-2.compute.internal>
2025-07-25 20:53:16 +0000

    updated python load of sample events.  waiting on GPT to create sample data correctly.

--- LoadSampleEvents_dev_current_time.py
+++ LoadSampleEvents_dev_current_time.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 def load_demo_events(csv_path: str, api_base_url: str):
     # Load combined CSV file
-    csv_path = "/home/ubuntu/sg_shippers_prototype/cryotrace_sg/Sample_Pickup___Dropoff_Data_23jul25.csv"
+    csv_path = "/home/ubuntu/sg_shippers_prototype/cryotrace_sg/Sample_Pickup___Dropoff_Data_25jul25_one_line_verify.csv"
 
     if not csv_path:
         print(f"❌ File not found: {csv_path}")
@@ -52,6 +52,7 @@ def load_demo_events(csv_path: str, api_base_url: str):
                 sm.destination_location_id,
                 sm.scheduled_ship_time,
                 sm.expected_receive_time,
+                sm.destination_contact_name,
                 sm.created_by_user_id,
                 CONCAT(origin.city, ', ', origin.state, ', ', origin.company_name, ' ', origin.company_address) AS origin,
                 CONCAT(dest.city, ', ', dest.state, ', ', dest.company_name, ' ', dest.company_address) AS destination
@@ -68,7 +69,6 @@ def load_demo_events(csv_path: str, api_base_url: str):
 
             weight_type = "pickup"
 
-
             cursor.execute("""
                 INSERT INTO container_weight_event (
                     manifest_id,
@@ -83,19 +83,17 @@ def load_demo_events(csv_path: str, api_base_url: str):
             """, (
                 pickup_data['manifest_id'],
                 weight_type,
-                datetime.strptime(pickup_data['weight_measured_at'], "%Y-%m-%dT%H:%M:%S"),
+                datetime.strptime(pickup_data['weight_measured_at'], "%Y-%m-%d %H:%M:%S"),
                 results_manifest[0]['origin_location_id'],
                 pickup_data['driver_user_id'],
                 pickup_data['measured_weight_kg'],
                 pickup_data['notes'],
-                datetime.strptime(row['dev_current_time_x'], "%Y-%m-%dT%H:%M:%S"),  # created_at
+                datetime.strptime(row['dev_current_time_x'], "%Y-%m-%d %H:%M:%S"),  # created_at
 
             ))
             conn.commit()            
             # also write the container_weight_event for this entry
-            print(f"Posting cwe from manifest data: {row['manifest_id_x']}  , {pickup_data}")
-
-
+            print(f"Posting cwe from pickup manifest data: {row['manifest_id_x']}  , {pickup_data}")
 
             cursor.execute("""
                 INSERT INTO pickup_event (
@@ -113,50 +111,97 @@ def load_demo_events(csv_path: str, api_base_url: str):
                 pickup_data['manifest_id'],
                 pickup_data['measured_weight_kg'],
                 pickup_data['weight_measured_at'],  #weight_measured_at,
-                datetime.strptime(pickup_data['actual_departure_at'], "%Y-%m-%dT%H:%M:%S"),  #actual_departure_at
+                datetime.strptime(pickup_data['actual_departure_at'], "%Y-%m-%d %H:%M:%S"),  #actual_departure_at
                 pickup_data['driver_user_id'],
                 pickup_data['image_path'],
                 pickup_data['notes'],
-                datetime.strptime(pickup_data['created_at'], "%Y-%m-%dT%H:%M:%S"),  # created_at
-                datetime.strptime(pickup_data['dev_current_time'], "%Y-%m-%dT%H:%M:%S"),  #dev_current_time
+                datetime.strptime(pickup_data['created_at'], "%Y-%m-%d %H:%M:%S"),  # created_at
+                datetime.strptime(pickup_data['dev_current_time'], "%Y-%m-%d %H:%M:%S"),  #dev_current_time
             ))
             conn.commit()
             print(f"Posting pickup for manifest {row['manifest_id_x']}  , {pickup_data}")
 
+        except Exception as e:
+            print("❌ Pickup Event failed:", e)
 
+
+        try:
+            dropoff_data = {
+                'manifest_id': row['manifest_id_y'],
+                'received_location_id': results_manifest[0]['destination_location_id'],
+                'received_contact_name':  results_manifest[0]['destination_contact_name'],
+                'actual_receive_time':  row['dev_current_time_y'],
+                'received_weight_kg':  row['received_weight_kg'],
+                'condition_notes':  row['condition_notes'],
+                'image_path':  'image_path',
+                'received_by_user_id':  1,
+                'created_at' : row['dev_current_time_y'],
+                'dev_current_time': row['dev_current_time_y']
+            }
+
+            
+            weight_type = "dropoff"
+
+            cursor.execute("""
+                INSERT INTO container_weight_event (
+                    manifest_id,     
+                    weight_type,
+                    event_time, 
+                    location_id,
+                    recorded_by_user_id,
+                    weight_kg,
+                    notes,
+                    created_at 
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s,  %s)
+            """, (
+                dropoff_data['manifest_id'],
+                weight_type,
+                datetime.strptime(dropoff_data['dev_current_time'], "%Y-%m-%d %H:%M:%S"),
+                dropoff_data['received_location_id'],
+                dropoff_data['received_by_user_id'],
+                dropoff_data['received_weight_kg'],
+                dropoff_data['condition_notes'],
+                datetime.strptime(dropoff_data['dev_current_time'], "%Y-%m-%d %H:%M:%S")  # created_at
+            ))
+            conn.commit()            
+            # also write the container_weight_event for this entry
+            print(f"Posting cwe from pickup manifest data: {row['manifest_id_x']}  , {pickup_data}")
+
+            cursor.execute("""
+                INSERT INTO dropoff_event (
+                    manifest_id,
+                    received_location_id,
+                    received_contact_name,
+                    actual_receive_time,
+                    received_weight_kg,
+                    condition_notes,
+                    image_path,
+                    received_by_user_id,
+                    created_at, 
+                    dev_current_time
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                dropoff_data['manifest_id'],
+                dropoff_data['received_location_id'],
+                dropoff_data['received_contact_name'],  #weight_measured_at,
+                datetime.strptime(dropoff_data['dev_current_time'], "%Y-%m-%d %H:%M:%S"),  #actual_departure_at
+                dropoff_data['received_weight_kg'],
+                dropoff_data['condition_notes'],
+                dropoff_data['image_path'],
+                dropoff_data['received_by_user_id'],
+                datetime.strptime(dropoff_data['dev_current_time'], "%Y-%m-%d %H:%M:%S"),  # created_at
+                datetime.strptime(dropoff_data['dev_current_time'], "%Y-%m-%d %H:%M:%S"), #dev_current_time
+            ))
+            conn.commit()
             cursor.close()
 
+            print({f"status": "Dropoff event recorded."})
 
-            return {"status": "Pickup event recorded."}
 
         except Exception as e:
-            print("❌ Pickup event failed:", e)
-            return
+            print("❌ Dropoff Event failed:", e)
+
             #return JSONResponse(status_code=500, content={"error": str(e)})
-
-
-        #print(f"Pickup response: {pickup_response.status_code} - {pickup_response.text}")
-
-        # --- Dropoff event ---
-        dropoff_files = {
-            'image_path': (row['image_path'], open(row['image_path'], 'rb')) if pd.notna(row['image_path']) else None
-        }
-        dropoff_data = {
-            'manifest_id': row['manifest_id_y'],
-            'received_location_id': row['received_location_id'],
-            'received_contact_name': row['received_contact_name'],
-            'received_weight_kg': row['received_weight_kg'],
-            'condition_notes': row['condition_notes'],
-            'received_by_user_id': row['received_by_user_id'],
-            'dev_current_time': row['dev_current_time_y']
-        }
-        print(f"Posting dropoff for manifest {row['manifest_id_y']}")
-        dropoff_response = requests.post(
-            f"{api_base_url}/api/dropoff-events",
-            data=dropoff_data,
-            files=dropoff_files if dropoff_files['image_path'] else None
-        )
-        print(f"Dropoff response: {dropoff_response.status_code} - {dropoff_response.text}")
 
 
 if __name__ == "__main__":
